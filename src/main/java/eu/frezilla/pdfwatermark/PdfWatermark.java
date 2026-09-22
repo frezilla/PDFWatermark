58b32e09b46cbc61d775affe8676e8cc8acb598d
fr3zill@ <34129644+frezilla@users.noreply.github.com>
2026-09-22 14:13:12 +0200

    Ajout de la gestion des paramètres passés en ligne de commande

--- src/main/java/eu/frezilla/pdfwatermark/PdfWatermark.java
+++ src/main/java/eu/frezilla/pdfwatermark/PdfWatermark.java
@@ -1,16 +1,68 @@
 package eu.frezilla.pdfwatermark;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+import picocli.CommandLine.ParameterException;
 
 public class PdfWatermark {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(PdfWatermark.class);
 
     public static void main(String[] args) throws Exception {
-        WatermarkConfig config = WatermarkConfig.createDefaultConfig("Confidentiel", "BaseRights", "9782330109646");
-        PdfWatermarkGenerator.getInstance().addWatermark(
-                Path.of("input.pdf"), 
-                Path.of("output.pdf"), 
-                config
-        );
-
+        PrintStream ps = System.out;
+        
+        Arguments arguments = new Arguments();
+        try {
+            new CommandLine(arguments).parseArgs(args);
+            if (arguments.isHelpRequest()) {
+                CommandLine.usage(arguments, ps);
+                System.exit(0);
+            }
+            
+            String documentId = arguments.getDocumentId();
+            String inputFile = arguments.getInputFile();
+            String label = arguments.getLabel();
+            String ouputFile = arguments.getOutputFile();
+            String recipient = arguments.getRecipient();
+            
+            LOGGER.info(
+                    String.format(
+                            """
+                            Arguments de lancement du traitement :
+                                Fichier source : %s
+                                Fichier destination : %s
+                                Texte principal du filigrane : %s
+                                Destinataire du documenent : %s
+                                Identifiant du document : %s
+                            """,
+                            inputFile,
+                            ouputFile,
+                            label, 
+                            recipient,
+                            documentId
+                    )
+            );
+            
+            WatermarkConfig config = WatermarkConfig.createDefaultConfig(label, recipient, documentId);
+            
+            try {
+                PdfWatermarkGenerator.getInstance().addWatermark(
+                    Path.of("input.pdf"), 
+                    Path.of("output.pdf"), 
+                    config
+                ); 
+            } catch (IOException e) {
+                LOGGER.error("Une erreur bloquante a été détectée au cours du traitement", e);
+                ps.println("Une erreur bloquate a été détectée au cours du traitement; consultez les logs pour obtenir plus d'informations.");
+                System.exit(-1);
+            }
+        } catch (ParameterException e) {
+            CommandLine.usage(arguments, ps);
+            System.exit(-1);
+        }
     }
 }
